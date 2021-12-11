7a52fe4b8202a385a072aab0ef952b855d733ce6
Tiara Dewangga <tirdewa@gmail.com>
2021-12-11 20:57:04 +0700

    Update form

--- main.go
+++ main.go
@@ -4,34 +4,77 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
-	"path"
 )
 
 func main() {
+
+	http.HandleFunc("/", handlerIndex)
+	http.HandleFunc("/form", handlerForm)
+	http.HandleFunc("/process", routeSubmitPost)
 	http.Handle("/static/",
 		http.StripPrefix("/static/",
-			http.FileServer(http.Dir("assets"))))
-	http.HandleFunc("/", handleIndex)
+			http.FileServer(http.Dir(http.Dir("assets")))))
 
-	fmt.Println("Server started at localhost:9000")
-	http.ListenAndServe(":9000", nil)
+	var address = "localhost:9000"
+	fmt.Printf("Server started at %s\n", address)
+	http.ListenAndServe(address, nil)
 }
 
-func handleIndex(w http.ResponseWriter, r *http.Request) {
-	var filePath = path.Join("views", "index.html")
-	var tmpl, err = template.ParseFiles(filePath)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+func handlerIndex(w http.ResponseWriter, r *http.Request) {
+	var tmpl = template.Must(template.ParseFiles(
+		"views/index.html",
+		"views/_header.html",
+	))
 	var data = map[string]interface{}{
-		"title": "Gogogo",
-		"name":  "Tiara",
+		"title": "Index",
 	}
 
-	err = tmpl.Execute(w, data)
+	var err = tmpl.ExecuteTemplate(w, "index", data)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+	}
+}
+
+func handlerForm(w http.ResponseWriter, r *http.Request) {
+	var tmpl = template.Must(template.ParseFiles(
+		"views/form.html",
+		"views/_header.html",
+	))
+	var data = map[string]interface{}{
+		"title": "Form",
+	}
+
+	var err = tmpl.ExecuteTemplate(w, "form", data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func routeSubmitPost(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		var tmpl = template.Must(template.New("result").ParseFiles(
+			"views/result.html",
+			"views/_header.html",
+		))
+
+		if err := r.ParseForm(); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		var name = r.FormValue("name")
+		var city = r.FormValue("city")
+
+		var data = map[string]string{
+			"name": name,
+			"city": city,
+		}
+
+		if err := tmpl.Execute(w, data); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		http.Error(w, "", http.StatusBadRequest)
 	}
 }
